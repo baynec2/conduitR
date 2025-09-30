555be57574866ddf823f6b6871044a45e6c3ad1a
Charlie Bayne <baynec2@gmail.com>
2025-09-30 16:03:10 -0700

    added protein name column, so that there is a brief description of each entry.

--- R/get_annotations_from_uniprot.R
+++ R/get_annotations_from_uniprot.R
@@ -13,7 +13,7 @@
 #' insulin <- get_annotations_from_uniprot("P01308")
 #' t <- get_annotations_from_uniprot("P47340")
 get_annotations_from_uniprot <- function(uniprot_ids,
-                                         columns = "accession,go,xref_kegg,xref_eggnog,cc_subcellular_location,cc_tissue_specificity,xref_cazy,xref_pfam,xref_interpro"
+                                         columns = "accession,protein_name,go,xref_kegg,xref_eggnog,cc_subcellular_location,cc_tissue_specificity,xref_cazy,xref_pfam,xref_interpro"
  ) {
 
   annotated_data <- annotate_uniprot_ids(uniprot_ids,

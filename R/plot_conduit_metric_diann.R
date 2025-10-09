500df96c3c5bed9455db36449300c04ef6c46a66
Charlie Bayne <baynec2@gmail.com>
2025-10-09 13:52:12 -0700

    added function to plot diann plots

---
+++ R/plot_conduit_metric_diann.R
@@ -0,0 +1,20 @@
+#' Plot Diann Sample Level Metrics for Conduit Object
+#'
+#' @param conduit_obj conduit object
+#' @param column_name column name to plot
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+plot_conduit_metric_diann = function(conduit_obj,column_name = "MS1.Signal"){
+
+  diann_metrics <- slot(conduit_obj, "metrics")$diann_stats
+
+  p1 = diann_metrics |>
+    ggplot2::ggplot(ggplot2::aes(x = File.Name, y = !!rlang::sym(column_name)))+
+    ggplot2::geom_point()+
+    ggplot2::geom_line(ggplot2::aes(group=1))
+
+  return(p1)
+}
